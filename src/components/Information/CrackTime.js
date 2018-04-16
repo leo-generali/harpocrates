7f2fd39924987b62c387bdd1638f763551f9b61e
Leo Generali <leogen17@gmail.com>
2018-04-16 13:37:36 -0400

    Add ability to conditionally apply color based on strength

--- src/components/Information/CrackTime.js
+++ src/components/Information/CrackTime.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const colors = {
+  second: '#eb2f06',
+  seconds: '#eb2f06',
+  minute: '#f8c291',
+  minutes: '#f8c291',
+  hour: '#fa983a',
+  hours: '#fa983a',
+  day: '#fad390',
+  days: '#fad390',
+  month: '#b8e994',
+  months: '#b8e994',
+  year: '#78e08f',
+  years: '#78e08f'
+}
+
 const Number = styled.p`
   font-weight: bold;
   font-size: 4rem;
@@ -10,6 +25,12 @@ const String = styled.span`
   margin-top: auto;
   text-align: end;
   font-size: 1.8rem;
+`;
+
+const Indicator = styled.div`
+  height: 3px;
+  width: 100%;
+  border-radius: 2px;
 `;
 
 const Container = styled.div`
