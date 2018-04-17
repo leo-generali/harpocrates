f344a7c5f665120c60fed3381d62984d7103e150
Leo Generali <leogen17@gmail.com>
2018-04-17 10:22:00 -0400

    Add minor styles to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+const OuterContainer = styled.div`
+  background-color: #ffffff;
+  height: 5rem;
+`;
+
+const InnerContainer = styled.div`
+  max-width: 96rem;
+  margin: 0 auto;
+  align-items: center;
+  display: flex;
+  height: 100%;
+`;
 
 const Header = ({ siteTitle }) => (
-  <div>
-    <h1>
-      <Link to='/'> {siteTitle} </Link>
-    </h1>
-  </div>
+  <OuterContainer>
+    <InnerContainer>
+      <h1>
+        <Link to='/'> {siteTitle} </Link>
+      </h1>
+      <h1>
+        <Link to='/about'> About </Link>
+      </h1>
+    </InnerContainer>
+  </OuterContainer>
 )
 
 export default Header;
